1d034d7104b16944680272c8676f808099272547
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-22 16:24:24 +0200

    Define dictionary with function

--- run.py
+++ run.py
@@ -28,6 +28,25 @@ def welcome_mess():
   #  def __init__(self, name):
    #     self.name=name  
 
+def Zodiac_sign():
+    """Define dictionary to show zodiac sign"""
+    zodiac_alphabets = {
+         'bh':'Sagittarius', 'f':'Sagittarius', 'dh':'Sagittarius', 
+         'p':'Virgo', 'tha':'Virgo', 
+         'dd':'Cancer', 'h':'Cancer', 
+         'g':'Aquarius', 's':'Aquarius', 'sh':'Aquarius', 
+         'kh':'Capricorn', 'j':'Capricorn', 
+         'd':'Pisces', 'ch':'Pisces', 'z':'Pisces', 'th':'Pisces', 
+         'a':'Aries', 'l':'Aries', 'e':'Aries', 'i':'Aries', 'o':'Aries', 
+         'k':'Gemini', 'chh':'Gemini', 'gh':'Gemini', 'q':'Gemini', 'c':'Gemini', 
+         'm':'Leo', 'tt':'Leo', 
+         'r':'Libra', 't':'Libra', 
+         'b':'Taurus', 'v':'Taurus', 'u':'Taurus', 'w':'Taurus', 
+         'n':'Scorpio', 'y':'Scorpio'
+    }
+    #user input convert to lowercase
+    lower_case = lower_case.lower()
+
 
 def user_name():
     """Display menu for user to select one option"""
@@ -37,10 +56,13 @@ def user_name():
         print("1. Discover Your Zodiac Identity Through Your Name!.")
         print("2. Read the importance of your Zodiac sign.")
         print("3. Exit program.")
-        
+        # User options for further steps 
         user_selection=input("Enter your choice (1, 2, or 3)")
-    
-        if user_selection =="1":
+        
+        if user_select == "1":
+            name_letter=input("Please enter first letter of your name")
+
+
 
 
 welcome_mess()
