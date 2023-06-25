406c323b145962432111134103d124f665f48c60
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-25 18:30:18 +0200

    Update content and spacing

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from colorama import Fore, Back, Style
 colorama.init() 
 
 def welcome_mess():
-    """Creates a welcome message"""
+    # Creates a welcome message
     print(Fore.CYAN + "######################################")
     print("#                                    #")
     print("#     Let's begin ZodiacJourney      #")
@@ -21,14 +21,14 @@ def welcome_mess():
     time.sleep(1)
     print("Let's start...")
     time.sleep(1)
-    print(Fore.CYAN + "^_^" + Style.RESET_ALL +"\n")
+    print(Fore.CYAN + "^_^" + Style.RESET_ALL + "\n")
     time.sleep(1)
   
 
 def zodiac_sign(letter):
     """Define dictionary to show zodiac sign,
-    Convert user input in lower case,
-    loop and if statment to show output"""
+    Convert user input in lowercase,
+    loop and if statement shows output"""
 
     zodiac_alphabets = {
          'bh':'Sagittarius', 'f':'Sagittarius', 'dh':'Sagittarius', 
@@ -44,7 +44,7 @@ def zodiac_sign(letter):
          'b':'Taurus', 'v':'Taurus', 'u':'Taurus', 'w':'Taurus', 
          'n':'Scorpio', 'y':'Scorpio', 'x':'No Zodiac sign for this letter'
     }
-    #User input convert to lowercase
+    # User input convert to lowercase
     lower_case = letter.lower() 
     # Look up zodiac sign of each letter
     for key, value in zodiac_alphabets.items():
@@ -52,13 +52,14 @@ def zodiac_sign(letter):
             time.sleep(1)
             return Fore.GREEN + f"Your Zodiac sign is '{value}'" + Style.RESET_ALL
         
-    return Fore.RED+"""No data found please enter correct character,
+    return Fore.RED + """No data found please enter the correct character,
     For Instance: 
-    Your name is 'John', enter 'j' or 'J'"""+ Style.RESET_ALL
+    Your name is 'John', enter 'j' or 'J'""" + Style.RESET_ALL
  
+
 def numerology(nume_letter):
     """Define dictionary to calculate numerology value,
-    check the value for name given by user,
+    check the value for a name given by the user,
     User input convert to lowercase"""
     numerology_alphabets = {
         'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 
@@ -73,6 +74,7 @@ def numerology(nume_letter):
     else:
         return 0 
     
+
 def addition_nume(user_name):
     #addition of name numerology value
     numerology_value = 0
@@ -82,7 +84,7 @@ def addition_nume(user_name):
             return Fore.RED + "Please enter only alphabets" + Style.RESET_ALL
         numerology_value += alpha_value
 
-    #addition of number base on letter
+    # Addition of number based on the letter
     while numerology_value > 9:
         letter_value = 0
         numerology_string = str(numerology_value)
@@ -92,14 +94,15 @@ def addition_nume(user_name):
     time.sleep(1)
     return Fore.GREEN + f"The numerology value of your name is '{numerology_value}'" + Style.RESET_ALL
 
+
 def user_name():
     """Display main menu for user to select one option,
     According to user choice give the relevant response"""
 
     while True:
-        #Error handle 
+        # Error handle 
         try:
-            #The main menu, gives user options to select.
+            # The main menu, gives user options to select.
             print("MENU:")
             print("1. Discover your Zodiac identity through your name.")
             print("2. Discover numerology value of your name.")
@@ -108,21 +111,21 @@ def user_name():
             user_selection = input("Enter your choice (1, 2, 3 or 4)\n")
 
             if user_selection == "1":
-                #Know the Zodiac sign
+                # Know the Zodiac sign
                 name_letter = input("Please enter the first letter of your name:\n")
                 rashi = zodiac_sign(name_letter)
                 print(rashi)
                 print('\n')
 
             elif user_selection == "2":
-                #Know numerology value of name
+                # Know numerology value of name
                 user_name = input("Enter your full name:")
                 nume_value = addition_nume(user_name)
                 print(nume_value)
                 print('\n')
 
             elif user_selection == "3":
-                #Read personality base on zodiac sign
+                # Read personality base on zodiac sign
                 file = open('importance.txt') 
                 importances = file.read()
                 file.close()
@@ -143,9 +146,6 @@ def user_name():
             print(Fore.RED + "Invalid input. Please enter a valid number." + Style.RESET_ALL)
 
 
-
 if __name__ == '__main__':
     welcome_mess()
-    user_name()
-    
-    
+    user_name()  
