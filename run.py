d527380962bb3aa0988ba61f73e0e3a807c0d43c
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-06-25 21:30:03 +0200

    Adjusted spacing

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import time
 import colorama
 from colorama import Fore, Back, Style
-colorama.init() 
+colorama.init()
 
 
 def welcome_mess():
@@ -32,29 +32,30 @@ def zodiac_sign(letter):
     loop and if statement shows output"""
 
     zodiac_alphabets = {
-         'bh' : 'Sagittarius', 'f' : 'Sagittarius', 'dh' : 'Sagittarius', 
-         'p' : 'Virgo', 'tha' : 'Virgo', 
-         'dd' : 'Cancer', 'h' : 'Cancer', 
-         'g' : 'Aquarius', 's' : 'Aquarius', 'sh' : 'Aquarius', 
-         'kh' : 'Capricorn', 'j' : 'Capricorn', 
-         'd' : 'Pisces', 'ch' : 'Pisces', 'z' : 'Pisces', 'th' : 'Pisces', 
-         'a' : 'Aries', 'l' : 'Aries', 'e' : 'Aries', 'i' : 'Aries', 'o' : 'Aries', 
-         'k' : 'Gemini', 'chh' : 'Gemini', 'gh' : 'Gemini', 'q' : 'Gemini', 'c' : 'Gemini', 
-         'm' : 'Leo', 'tt' : 'Leo', 
-         'r' : 'Libra', 't' : 'Libra', 
-         'b' : 'Taurus', 'v' : 'Taurus', 'u' : 'Taurus', 'w' : 'Taurus', 
-         'n' : 'Scorpio', 'y' : 'Scorpio', 'x' : 'No Zodiac sign for this letter'
+        'bh': 'Sagittarius', 'f': 'Sagittarius', 'dh': 'Sagittarius',
+        'p': 'Virgo', 'tha': 'Virgo',
+        'dd': 'Cancer', 'h': 'Cancer',
+        'g': 'Aquarius', 's': 'Aquarius', 'sh': 'Aquarius',
+        'kh': 'Capricorn', 'j': 'Capricorn',
+        'd': 'Pisces', 'ch': 'Pisces', 'z': 'Pisces', 'th': 'Pisces',
+        'a': 'Aries', 'l': 'Aries', 'e': 'Aries', 'i': 'Aries', 'o': 'Aries',
+        'k': 'Gemini', 'chh': 'Gemini', 'gh': 'Gemini',
+        'q': 'Gemini', 'c': 'Gemini',
+        'm': 'Leo', 'tt': 'Leo',
+        'r': 'Libra', 't': 'Libra',
+        'b': 'Taurus', 'v': 'Taurus', 'u': 'Taurus', 'w': 'Taurus',
+        'n': 'Scorpio', 'y': 'Scorpio', 'x': 'No Zodiac sign for this letter'
     }
     # User input convert to lowercase
-    lower_case = letter.lower() 
+    lower_case = letter.lower()
     # Look up zodiac sign of each letter
     for key, value in zodiac_alphabets.items():
         if key == lower_case:
             time.sleep(1)
-            return Fore.GREEN + f"Your Zodiac sign is '{value}'" + Style.RESET_ALL
-        
+            return Fore.GREEN + f"Your Zodiac sign is '{value}'"+ Style.RESET_ALL
+
     return Fore.RED + """No data found please enter the correct character,
-    For Instance: 
+    For Instance:
     Your name is 'John', enter 'j' or 'J'""" + Style.RESET_ALL
 
 
@@ -63,21 +64,20 @@ def numerology(nume_letter):
     check the value for a name given by the user,
     User input convert to lowercase"""
     numerology_alphabets = {
-        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 
-        'h': 8, 'i': 9, 'j': 1, 'k': 2, 'l': 3, 'm': 4, 'n': 5, 
-        'o': 6, 'p': 7, 'q': 8, 'r': 9,'s': 1, 't': 2, 'u': 3, 
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,
+        'h': 8, 'i': 9, 'j': 1, 'k': 2, 'l': 3, 'm': 4, 'n': 5,
+        'o': 6, 'p': 7, 'q': 8, 'r': 9, 's': 1, 't': 2, 'u': 3,
         'v': 4, 'w': 5, 'x': 6, 'y': 7, 'z': 8
     }
-    
     nume_letter = nume_letter.lower()
     if nume_letter in numerology_alphabets:
         return numerology_alphabets[nume_letter]
     else:
-        return 0 
-    
+        return 0
+
 
 def addition_nume(user_name):
-    #addition of name numerology value
+    # Addition of name numerology value
     numerology_value = 0
     for each_letter in user_name:
         alpha_value = numerology(each_letter)
@@ -93,7 +93,7 @@ def addition_nume(user_name):
             letter_value += int(number)
             numerology_value = letter_value
     time.sleep(1)
-    return Fore.GREEN + f"The numerology value of your name is '{numerology_value}'" + Style.RESET_ALL
+    return Fore.GREEN + f"The numerology value of your name is'{numerology_value}'" + Style.RESET_ALL
 
 
 def user_name():
@@ -101,7 +101,7 @@ def user_name():
     According to user choice give the relevant response"""
 
     while True:
-        # Error handle 
+        # Error handle
         try:
             # The main menu, gives user options to select.
             print("MENU:")
@@ -113,7 +113,7 @@ def user_name():
 
             if user_selection == "1":
                 # Know the Zodiac sign
-                name_letter = input("Please enter the first letter of your name:\n")
+                name_letter = input("Please enter the first letter of name:\n")
                 rashi = zodiac_sign(name_letter)
                 print(rashi)
                 print('\n')
@@ -127,7 +127,7 @@ def user_name():
 
             elif user_selection == "3":
                 # Read personality base on zodiac sign
-                file = open('importance.txt') 
+                file = open('importance.txt')
                 importances = file.read()
                 file.close()
                 print(importances)
@@ -135,12 +135,12 @@ def user_name():
 
             elif user_selection == "4":
                 # Exit
-                print(Fore.CYAN + "I appreciate your utilization of this program!" + Style.RESET_ALL)
-                break  
-            
+                print(Fore.CYAN + "I appreciate youfor using this program!" + Style.RESET_ALL)
+                break
+
             else:
                 # user selection is not valid
-                print(Fore.RED + "invalid choice. Please select again." + Style.RESET_ALL) 
+                print(Fore.RED + "invalid choice. Please select again." + Style.RESET_ALL)
                 print('\n')
 
         except ValueError:
@@ -149,4 +149,4 @@ def user_name():
 
 if __name__ == '__main__':
     welcome_mess()
-    user_name()  
+    user_name()
